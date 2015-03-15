ea12a52bd82196432861d9f433b7b0fc59bb2666
Dominik Stodolny <d.stodolny@gmail.com>
2015-03-15 03:57:41 +0100

    add new post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 3 }
-  validates :url, presence: true, url: true
+  validates :title, presence: true, length: { minimum: 3, maximum: 255 }
+  validates :url, presence: true, url: true, length: { minimum: 3, maximum: 255 }
+  validates_presence_of :user_id
   belongs_to :user
   has_many :comments
 end
